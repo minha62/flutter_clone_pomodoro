2a9ab7c23bbc19452c5ab5a5c806e18cca2d183e
Sumin Ha <minha62@ewhain.net>
2023-09-19 15:13:37 +0900

    Pomodoro initial UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_pomo/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSwatch(backgroundColor: const Color(0xFFE7626C)),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // Column의 주축은 수직 방향
-            children: [
-              // Icon눌러서 title을 숨기면 위젯 트리에서 제거되고 dispose() 실행됨
-              // 다시 Icon 눌러서 title 생기면 initState()와 build() 실행됨
-              showTitle ? const MyLargeTitle() : const Text('nothing'),
-              IconButton(
-                  onPressed: toggleTitle,
-                  icon: const Icon(Icons.remove_red_eye))
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  int count = 0; // 대부분의 경우 initState() 사용하지 않고 여기서 상태 변수 선언
-
-  @override
-  void initState() {
-    // initState()는 항상 build() 전에 실행됨
-    super.initState();
-    print('initState');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print('dispose');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge!.color,
-      ),
+      home: const HomeScreen(),
     );
   }
 }
